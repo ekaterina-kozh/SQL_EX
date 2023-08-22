0b016475cb924b0693df157dd594396033f0cc4e
ekaterina-kozh <80469111+ekaterina-kozh@users.noreply.github.com>
2023-08-22 16:44:55 +0300

    CreateStructure
    Added tables

---
+++ CreateStructure.sql
@@ -0,0 +1,21 @@
+CREATE TABLE dbo.SKU
+	(ID int PRIMARY KEY IDENTITY, 
+	Code as ('s'+ID) UNIQUE ,
+	Name nvarchar(100))
+
+CREATE TABLE dbo.Family 
+	(ID int PRIMARY KEY IDENTITY,
+	SurName nvarchar(100), 
+	BurgerValue decimal)
+
+CREATE TABLE dbo.Basket
+	(ID int PRIMARY KEY IDENTITY, 
+	ID_SKU int,
+	ID_Famaly int,
+	Quantity decimal check (Quantity>0),
+	Value decimal CHECK (Quantity>0), 
+	PurchaseDate date DEFAULT CURRENT_TIMESTAMP, 
+	DiscountValue decimal,
+	FOREIGN KEY (ID_SKU)  REFERENCES dbo.SKU (ID),
+	FOREIGN KEY (ID_Famaly)  REFERENCES dbo.Family (ID))
+
