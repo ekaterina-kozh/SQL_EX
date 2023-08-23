9fbff67cded88a2b7ffe366521d9af35736e3d4d
ekaterina-kozh <80469111+ekaterina-kozh@users.noreply.github.com>
2023-08-23 09:16:10 +0300

    Update CreateStructure.sql

--- CreateStructure.sql
+++ CreateStructure.sql
@@ -11,11 +11,11 @@ CREATE TABLE dbo.Family
 CREATE TABLE dbo.Basket
 	(ID int PRIMARY KEY IDENTITY, 
 	ID_SKU int,
-	ID_Famaly int,
+	ID_Family int,
 	Quantity decimal(18,2) check (Quantity>0),
 	Value decimal(18,2) CHECK (Value>0), 
 	PurchaseDate date DEFAULT CURRENT_TIMESTAMP, 
-	DiscountValue decimal,
+	DiscountValue decimal(18,2),
 	FOREIGN KEY (ID_SKU)  REFERENCES dbo.SKU (ID),
 	FOREIGN KEY (ID_Famaly)  REFERENCES dbo.Family (ID))
 
